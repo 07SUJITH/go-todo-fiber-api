fc8220adda1a5a23f37e19a8131a118059d674ad
07SUJITH <tssujith2002@gmail.com>
2024-10-16 21:11:11 +0530

    Refactor: Update main.go to conditionally load .env file and serve static files in production environment

--- main.go
+++ main.go
@@ -24,11 +24,16 @@ var collection *mongo.Collection
 
 func main() {
 	fmt.Println("Hello, World!")
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+
+	}
+	
+	
 	MONGODB_URI := os.Getenv("MONGODB_URI")
 	
 	clientOptions := options.Client().ApplyURI(MONGODB_URI)
@@ -49,6 +54,10 @@ func main() {
 	collection = client.Database("golang_db").Collection("todos")
 
 	app := fiber.New()
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "http://localhost:5173",
+	// 	AllowHeaders: "Origin, Content-Type, Accept",
+	// }))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
@@ -58,6 +67,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
